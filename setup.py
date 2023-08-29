c731b38e2de907551e343716cbfc164fe8f840bc
ark231 <arklp20909@gmail.com>
2023-08-29 18:58:31 +0900

    change: src-layoutに変更

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setuptools.setup(
         name="protein_file_format",
         version="0.1.0",
         setup_requires=['setuptools-protobuf'],
-        protobufs=[Protobuf('protein.proto')],
+        protobufs=[Protobuf('src/protein/protein.proto')],
 )
